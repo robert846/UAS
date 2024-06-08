e537a47277a65f2cf7fe2515187154b35f6e089f
robert846 <robertwirawan41@gmail.com>
2024-06-08 21:44:47 +0700

    membuat login dan seeder database

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.dashboard');
 });
+
+Route::get('/login', function () {
+    return view('pages.auth.login');
+});
+
+Route::get('/user', function () {
+    return view('pages.user.index');
+});
