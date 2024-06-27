808d6a98c3c8aef266446147f4ae90f5e4de03b6
robert846 <robertwirawan41@gmail.com>
2024-06-27 21:08:59 +0700

    menmbahkan menu daftar transfer, mutasi rekening, edit dashboard, mengganti nama akun

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Stisla</a>
+            <a href="index.html">MBS</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">St</a>
@@ -14,11 +14,7 @@
                 <ul class="dropdown-menu">
                     <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
                         <a class="nav-link"
-                            href="{{ url('dashboard-general-dashboard') }}">General Dashboard</a>
-                    </li>
-                    <li class="{{ Request::is('dashboard-ecommerce-dashboard') ? 'active' : '' }}">
-                        <a class="nav-link"
-                            href="{{ url('dashboard-ecommerce-dashboard') }}">Ecommerce Dashboard</a>
+                            href="{{ url('home') }}">General Dashboard</a>
                     </li>
                 </ul>
             </li>
--- resources/views/pages/masters/index.blade.php
+++ resources/views/pages/masters/index.blade.php
@@ -13,7 +13,48 @@
             </div>
 
             <div class="section-body">
-            </div>
+                <h2 class="section-title">M-Info</h2>
+                <p class="section-lead">
+                </p>
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Informasi</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Nomor Rekening</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <input type="text"
+                                            class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Tanggal</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <input type="text"
+                                            class="form-control">
+                                    </div>
+                                </div>
+                                   <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Saldo</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <input type="text"
+                                            class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <button class="btn btn-primary">Kembali</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </section>
     </div>
 @endsection
